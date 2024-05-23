c2a106628d2ad7ce0b5145cb1a37dcb3b5802d87
Asyary Raihan <asyaryraihan@gmail.com>
2024-05-24 03:37:59 +0700

    Init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// Global variables
+bool isQuit = false, doneLoading = false, doneReading = false;
+
+// Prototypes
+void quit();
+
+void quit() {
+	isQuit = true;
+	system("cls");
+	cout << "Terima kasih sudah menggunakan layanan Nama Warnet.\n\n"; // @keboooooo ingpokan nama warnet yang terkenal+pendek (edit dikit biar ga sama)
+	exit(0);
+}
+
+void init() {
+
+}
+
+int main() {
+	atexit(quit);
+	signal(SIGINT, exit);
+	init();
+
+	return 0;
+}
