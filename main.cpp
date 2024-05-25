86e5e9c224939dbb135ec8cf3650d796eefc7515
Asyary Raihan <asyaryraihan@gmail.com>
2024-05-25 23:40:22 +0700

    Add functionality

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <thread>
 
 #include "include/utils.h"
 #include "include/sha256.h"
 
 using namespace std;
+using namespace std::this_thread;
 
 // Global variables
 bool isQuit = false, doneLoading = false, doneReading = false;
@@ -12,15 +14,68 @@ bool isQuit = false, doneLoading = false, doneReading = false;
 // Prototypes
 void quit();
 
+void menu(int num) {
+
+}
+
+void readDB() {
+	doneReading = true;
+}
+
+void loadingScr() {
+	char spinner[4] = {'|', '/', '-', '\\'};
+	int counter  = 0;
+	cout << "Loading...  ";
+	while(!doneLoading || !doneReading) {
+		cout << '\b' << spinner[counter];
+		counter = (counter+1) % 4;
+		delay(200);
+	}
+}
+
+void greet() {
+	cls();
+	string banner = R"(
+       ___________      
+      |.---------.|     
+      ||         ||     ▒██   ██▒  ▄████  ▄▄▄     ▄▄▄█████▓▓█████ 
+      ||         ||     ▒▒ █ █ ▒░ ██▒ ▀█▒▒████▄   ▓  ██▒ ▓▒▓█   ▀ 
+      ||         ||     ░░  █   ░▒██░▄▄▄░▒██  ▀█▄ ▒ ▓██░ ▒░▒███   
+      |'---------'|      ░ █ █ ▒ ░▓█  ██▓░██▄▄▄▄██░ ▓██▓ ░ ▒▓█  ▄ 
+       `)__ ____('      ▒██▒ ▒██▒░▒▓███▀▒ ▓█   ▓██▒ ▒██▒ ░ ░▒████▒
+       [=== -- o ]--.   ▒▒ ░ ░▓ ░ ░▒   ▒  ▒▒   ▓▒█░ ▒ ░░   ░░ ▒░ ░
+     __'---------'__ \  ░░   ░▒ ░  ░   ░   ▒   ▒▒ ░   ░     ░ ░  ░
+    [::::::::::: :::] )  ░    ░  ░ ░   ░   ░   ▒    ░         ░   
+     `""'"""""'""""`/T\  ░    ░        ░       ░  ░           ░  ░
+                    \_/ 
+)";
+	cout << banner << "\nSelamat datang di aplikasi xGate Warnet!\n\n";
+	pause();
+	menu(1);
+}
+
 void quit() {
 	isQuit = true;
-	system("cls");
+	cls();
 	cout << "Terima kasih sudah menggunakan layanan xGate Warnet.\n\n";
 	exit(0);
 }
 
 void init() {
 	cls();
+	showCursor(false);
+	thread t1(loadingScr);
+	thread t2(readDB);
+	thread t3([]() { // lambda function uhuy
+		delay(2500); // minimum loading time 2.5 detik
+		doneLoading = true;
+	});
+	t1.join();
+	t2.join();
+	t3.join();
+	showCursor(true);
+	doneLoading = false; doneReading = false;
+	return greet();
 }
 
 int main() {
